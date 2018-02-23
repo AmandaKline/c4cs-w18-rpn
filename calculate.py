b9ac7e318a0e777ef2399070890def57215f6304
Amanda <klineama@umich.edu>
2018-02-23 10:49:07 -0500

    Catching up on code. Doesn't work.

--- calculate.py
+++ calculate.py
@@ -1,25 +1,29 @@
 #!/usr/bin/env python3
 
+import operator
+
+operators = {
+	'+': operator.add,
+	'-': operator.sub,
+}
+
 def calculate(arg):
 	stack = list()
 	for token in arg.split():
 		try:
 			value = int(token)
 			stack.append(value)
-		except:
-			if token == '+'
-				arg1 = stack.pop()
-				arg2 = stack.pop()
-				result = arg1 + arg 2
-				stack.append(result)
-			elif token == '-'
-				arg2 = stack.pop()
-				arg1 = stack.pop()
-				result = arg1 - arg 2
-				stack.append(result)
-			else:
-				print('Not an operator.')
+		except ValueError:
+			function = operators[token]
+			arg2 = stack.pop()
+			arg1 = stack.pop()
+			result = function(arg1, arg2)
+			stack.append(result)
 		print(stack)
 
 	return stack.pop()
+
+def main():
+	while True:
+		print(calculate(input('rpn calc> ')))
 
