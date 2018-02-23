e7fee470164dc3d327c6005eaeb36996ce1b9031
Amanda <klineama@umich.edu>
2018-02-23 10:29:36 -0500

    Starting to copy code from Darden's example

---
+++ Makefile
@@ -0,0 +1,13 @@
+//something for 3 lines
+
+class TestBasics(unittest.TestCase):
+	def test_add(self):
+		result = rpn.calculate('1 1 +')
+		self.assertEqual(2, result)
+	def test_adds(self):
+		result = rpn.calculat('1 1 + 2 +')
+		self.assertEqual(4, result)
+	def test_subtract(self):
+		result = rpn.calculate('5 2 -')
+		self.assertEqual(3, result)
+
---
+++ calculate.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+def calculate(arg):
+	stack = list()
+	for token in arg.split():
+		try:
+			value = int(token)
+			stack.append(value)
+		except:
+			if token == '+'
+				arg1 = stack.pop()
+				arg2 = stack.pop()
+				result = arg1 + arg 2
+				stack.append(result)
+			elif token == '-'
+				arg2 = stack.pop()
+				arg1 = stack.pop()
+				result = arg1 - arg 2
+				stack.append(result)
+			else:
+				print('Not an operator.')
+		print(stack)
+
+	return stack.pop()
+
