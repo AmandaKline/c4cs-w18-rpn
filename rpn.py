f017e2694f7315505ca5c352e260c64cb0626837
Amanda <klineama@umich.edu>
2018-02-23 10:53:33 -0500

    All caught up! Cann add and subtract

--- rpn.py
+++ rpn.py
@@ -1,7 +1,27 @@
 #!/usr/bin/env python 3
 
+import operator
+
+operators = {
+	'+': operator.add,
+	'-': operator.sub,
+}
+
 def calculate(arg):
-	pass
+	stack = list()
+	for token in arg.split():
+		try:
+			value = int(token)
+			stack.append(value)
+		except ValueError:
+			function = operators[token]
+			arg2 = stack.pop()
+			arg1 = stack.pop()
+			result = function(arg1, arg2)
+			stack.append(result)
+		print(stack)
+
+	return stack.pop()
 
 def main():
 	while True:
